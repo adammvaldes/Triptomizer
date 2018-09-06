b05d1ec9cc67a16b749639d2dd7ff1184716db28
vstepanuga <42558356+vstepanuga@users.noreply.github.com>
2018-09-05 18:10:40 -0600

    Testing changes to distance class

---
+++ server/src/main/java/com/tripco/t13/server/Distance.java
@@ -0,0 +1,16 @@
+package com.tripco.t13.server;
+
+import com.google.gson.Gson;
+
+public class Distance {
+    Distance(String jsonObj) {
+
+    }
+
+    public String distance;
+
+    public String getDistanceObject() {
+        //TODO
+        return "todo";
+    }
+}
