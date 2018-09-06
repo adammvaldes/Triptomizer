030f356bd477f3fa3af224e0df4c6c8d95568c36
vstepanuga <42558356+vstepanuga@users.noreply.github.com>
2018-09-05 18:44:46 -0600

    Distance class 0.1

--- server/src/main/java/com/tripco/t13/server/Distance.java
+++ server/src/main/java/com/tripco/t13/server/Distance.java
@@ -1,14 +1,37 @@
 package com.tripco.t13.server;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import jdk.nashorn.internal.parser.JSONParser;
 import spark.Request;
 
 public class Distance {
+    String type;
+    int version;
+    Object origin;
+    Object destination;
+    String units;
+    int distance;
+    Gson gson = new Gson();
+
     public Distance(Request request) {
+
+        JsonParser jsonParser = new JsonParser();
+        JsonElement requestBody = jsonParser.parse(request.body());
+
+        Distance distance = gson.fromJson(requestBody, Distance.class);
+
+        distance.getDistanceNum();
+    }
+
+
+    public int getDistanceNum() {
+        return 10;
     }
 
     public String getDistanceObject() {
-
-        return "{ it}";
+        return gson.toJson(distance);
     }
 
 }
