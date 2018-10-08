f6843b81702e874052244baa9810a13b3aac2032
leejr0 <31223794+leejr0@users.noreply.github.com>
2018-10-08 16:17:52 -0600

    Added a couple Search classes and methods to handle the upcoming search request from the client

---
+++ server/src/main/java/com/tripco/t13/server/performSearch.java
@@ -0,0 +1,56 @@
+package com.tripco.t13.server;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import spark.Request;
+import java.util.*;
+
+public class performSearch {
+
+    private Search search;
+    private boolean isCorrectFormat; //verify correct format of POST request
+
+
+    public performSearch(Request request){
+        isCorrectFormat=false;
+
+        // extract the information from the body of the request.
+        JsonParser jsonParser = new JsonParser();
+        JsonElement requestBody = jsonParser.parse(request.body());
+
+        //Converting to a Java class
+        Gson gson = new Gson();
+        try {
+            search = gson.fromJson(requestBody, Search.class);
+            isCorrectFormat = validateSearchRequestFormat(search);
+
+
+
+        } catch (Exception e) {
+
+            isCorrectFormat = false;
+        }
+    }
+
+    public boolean validateSearchRequestFormat(Search search) {
+        //check if format of request if correct: type:"search" and version 3
+        if(     Objects.equals(search.type, "search") &&
+                (search.version==3)
+        ){
+            return true;
+        }
+
+        return false;
+    }
+    public String getSearchJson () {
+        Gson gson = new Gson();
+        if(isCorrectFormat){
+            return gson.toJson(search);
+        }
+        else{
+            return "{}"; //return {} if incorrect request format
+        }
+
+    }
+}
