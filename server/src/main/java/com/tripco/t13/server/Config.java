c738cd86b43b0b151a61f7154402a0a15c34da06
vstepanuga <42558356+vstepanuga@users.noreply.github.com>
2018-10-09 19:59:13 -0600

    Changed config to represent sprint 3.

--- server/src/main/java/com/tripco/t13/server/Config.java
+++ server/src/main/java/com/tripco/t13/server/Config.java
@@ -7,15 +7,29 @@ import java.util.List;
 
 public class Config {
 
-  private short version = 2;
+  private short version = 3;
   private String type = "config";
 
   private List<String> units = Arrays.asList("miles","kilometers","nautical miles","user defined");
+  private List<OptimizationConfig> optimization;
 
   static String getConfig() {
     Config conf = new Config();
+    OptimizationConfig option1 = new OptimizationConfig();
+    OptimizationConfig option2 = new OptimizationConfig();
+
+    option1.label = "none";
+    option1.description = "The trip is not optimized.";
+
+    option2.label = "short";
+    option2.description = "Nearest neighbor.";
+
+    conf.optimization = Arrays.asList(option1, option2);
+
     Gson gson = new Gson();
+
 
     return gson.toJson(conf);
   }
+
 }
