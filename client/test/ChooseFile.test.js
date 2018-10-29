d7fbd97e7d6fd63f2124bb4fa992e9cbe3077d4d
leejr0 <31223794+leejr0@users.noreply.github.com>
2018-10-29 17:04:43 -0600

    Added the ability to start trips from scratch in the ChooseFile. Added a calculate distance button for the client to use. Added a testing file for ChooseFile Modified the testing file for Trip. Deleted some comments, cleaned code

---
+++ client/test/ChooseFile.test.js
@@ -0,0 +1,46 @@
+import './enzyme.config.js'                   // (1)
+import React from 'react'
+import { mount , shallow} from 'enzyme'              // (2)
+import ChooseFile from '../src/components/Application/ChooseFile'
+import {Button} from "reactstrap";
+
+
+const testProps = {
+    trip: {
+        type: "trip",
+        version: "3",
+        title: "Stuffity",
+        options: {
+            units: "miles",
+            unitName: "miles",
+            unitRadius: "3959",
+            optimization: "none"
+        },
+        places: [],
+        distances: [],
+        map: ''
+    }
+};
+
+function updateTFFI(tffi) {
+    testProps.trip = tffi;
+}
+
+
+function testRender() {
+    const fileWrapper = mount((
+        <ChooseFile
+            trip={testProps.trip}
+            updateTFFI={updateTFFI}
+        />
+    ));
+
+    let actualButtonTexts = fileWrapper.find('Button').map((button)=>{
+        return button.text();
+    });
+
+    let expectedButtonTexts = [' Search for locations ', 'Search', 'Add destination to Trip',' Add your own location ', 'Add to trip'];
+    expect(actualButtonTexts).toEqual(expectedButtonTexts)
+}
+
+test('Testing plan function in Trip.js', testRender);
