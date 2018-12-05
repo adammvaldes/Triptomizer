7b4c0a829b11a5d85aa50dfe8979274949920f93
leejr0 <31223794+leejr0@users.noreply.github.com>
2018-12-05 16:37:13 -0700

    Rendering Map on Default

--- client/src/components/Application/Info.js
+++ client/src/components/Application/Info.js
@@ -70,18 +70,3 @@ export default class Info extends Component {
   }
 }
 //https://getavataaars.com/?accessoriesType=Prescription02&clotheColor=Black&clotheType=Hoodie&eyeType=Surprised&eyebrowType=UpDown&facialHairColor=Black&facialHairType=Blank&hairColor=Black&mouthType=Serious&skinColor=DarkBrown&topType=ShortHairTheCaesar
-/*
-<p className="lead">"Want to travel far and wide?"</p>
-            <ol >
-              <li>
-                Choose options for trip planning, information to display about locations,
-                and how the trip map and itinerary should be saved.</li>
-              <li>
-                Choose your destinations by loading existing sets of destinations or
-                find more in an extensive database of locations worldwide.</li>
-              <li>
-                Plan the trip with the options you selected.
-                Review and revise the trip origin and order.
-                Save the trip map and itinerary for future reference.</li>
-            </ol>
- */
